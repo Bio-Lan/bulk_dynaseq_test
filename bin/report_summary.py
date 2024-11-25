1db4b3ea0e22d5cdea05c0cb47168a1cfd554c22
Bio-lan <hikio@qq.com>
2024-11-25 15:59:50 +0800

    pipeline modify

--- bin/report_summary.py
+++ bin/report_summary.py
@@ -143,7 +143,6 @@ if __name__ == "__main__":
     parser.add_argument("--sub_stats", help="substitution csv file")
     parser.add_argument("--sample_raw", help="qutan sample raw csv file")
     parser.add_argument("--sample_filter", help="qutan sample filter csv file")
-    parser.add_argument("--DRACH_display", default=3, type=int, help="how many DRACH display")
     args = parser.parse_args()
 
     Report_Summary(args).run()
